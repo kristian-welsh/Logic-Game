1d0c5d6c995a1147dfbd15900479fa5c574dd430
harry2469 <kriswelsh@gmail.com>
2014-11-14 19:20:40 +0000

    Implemented very simple testing harness

--- src/test.js
+++ src/test.js
@@ -1,3 +1,24 @@
+// ExampleTest.js
+function ExampleTest() {
+  this.test_thingy = function() {
+    assert(false)
+  }
+}
+
+ExampleTest.getTests = function() {
+  var tests = [
+    "test_thingy"]
+  return tests
+}
+
+// TestRunner.js
+function runTests(testClass) {
+  var tests = testClass.getTests()
+  for (var i = 0; i < tests.length; ++i)
+      new testClass()[tests[i]]()
+}
+
+// Util.js
 function assert(condition, failMessage) {
   message = "An assertion has failed with no error message"
   if(failMessage)
@@ -5,3 +26,6 @@ function assert(condition, failMessage) {
   if(!condition)
     throw message
 }
+
+// this file
+runTests(ExampleTest)
